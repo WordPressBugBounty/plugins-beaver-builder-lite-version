cef2ccd08d4393aff9ddbc862b2b1bd3b5dd411a
Sean Murphy <sean@iamseanmurphy.com>
2024-10-09 18:48:46 +0000

    2.8.4.1

--- beaver-builder-lite-version/includes/updater-config.php
+++ beaver-builder-lite-version/includes/updater-config.php
@@ -3,7 +3,7 @@
 if ( class_exists( 'FLUpdater' ) ) {
 	FLUpdater::add_product(array(
 		'name'    => 'Beaver Builder Plugin (Lite Version)',
-		'version' => '2.8.3.9',
+		'version' => '2.8.4.1',
 		'slug'    => 'bb-plugin',
 		'type'    => 'plugin',
 	));
